0a21d706c06c7719009fa6d08ba245988f87eaa6
david cochran <about.trout@gmail.com>
2019-01-23 09:43:11 -0800

    fix build script

--- build_and_flash.sh
+++ build_and_flash.sh
@@ -3,10 +3,21 @@
 TARGET=atmega32u4
 KEYMAPFILE=hhkb.hex
 
+# On some machines, I need to `sudo dfu-programmer`.
+sudo -v
+
 make clean && make
 
+echo "=================================="
+echo "Ready to flash. Please put keyboard in bootloader mode"
+echo "Sleeping for 15 seconds..."
+echo "=================================="
+
+sleep 15
+
 if [[ $? -eq 0 ]]; then
-  dfu-programmer $TARGET erase
-  dfu-programmer $TARGET flash $KEYMAPFILE
-  dfu-programmer $TARGET reset
+  sudo -- sh -c "
+    dfu-programmer $TARGET erase && \
+    dfu-programmer $TARGET flash $KEYMAPFILE && \
+    dfu-programmer $TARGET reset"
 fi
